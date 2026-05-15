b2be240982ad02ed164735e6c5603693534a669b
xXHanzellXx <165698536+xXHanzellXx@users.noreply.github.com>
2026-05-15 14:36:05 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import bcrypt
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["flask_login"]
+usuarios = db["usuarios"]
+
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/")
+def home():
+
+    if "usuario" not in session:
+        return redirect(url_for("login"))
+    
+    return render_template(
+        "home.html",
+        usuario=session["usuario"]
+    )
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    
+    if request.method == "POST":
+        
+        nombre = request.form["nombre"]
+        apellido1 = request.form["apellido1"]
+        apellido2 = request.form["apellido2"]
+        correo = request.form["correo"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        foto = request.form["foto"]
+        usuario_existente = usuarios.find_one({
+                "username" : username
+            })
+
+        if usuario_existente:
+                return "El usuario ya existe"
+
+        foto_path = ""
+
+        if foto: 
+            foto_path = os.path.join(
+                app.config["UPLOAD_FOLDER"],
+                foto.filename
+                )
+        foto.save(foto_path)
+
+        password_hash = bcrypt.hashpw(
+             password.encode("utf-8"),
+             bcrypt.gensalt()
+        )
+
+        usuarios.insert_one({
+            "nombres": nombre,
+            "apellido1": apellido1,
+            "apellido2": apellido2,
+            "correo": correo,
+            "username": username,
+            "foto": foto.filename,
+            "password":password_hash 
+        })
+
+        return redirect(url_for("login"))
+    
+    return render_template("register.html")
+
+@app.route("/login",methods=["GET", "POST"])
+def login(): 
+     if request.method == "POST":
+          username = request.form["username"]
+          password = request.form["password"]
+
+          usuario = usuarios.find_one({
+               "username": username 
+          })
+
+          if not usuario:
+               return redirect(url_for("register"))
+          
+          if bcrypt.checkpw(
+               password.encode("utf-8"),
+               usuario["password"]
+          ):
+               session("usuario") == {
+                    "nombre": usuario["nombre"],
+                    "username": usuario["username"],
+                    "foto": usuario["foto"]
+               }
+
+               return redirect(url_for("home"))
+          
+          else:
+               return "Contraseña incorrecta"
+
+def login():
+     return render_template("/login.html")
+
+@app.route("/logout")
+def logout():
+     
+     session.clear()
+
+     return redirect(url_for("login"))
+
+if __name__ == "__main__":
+     app.runU(debug=True)
